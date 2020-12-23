4ee71fbc4996645c93db9ac76485e26cec3ffa7a
play.js <git@gituser.com>
2020-12-22 18:28:19 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const map= require("./map")
+
+http.createServer(function (req, res) {
+  fs.readFile('map.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
---
+++ map.js
@@ -0,0 +1,8 @@
+//const http=require("http")
+
+function outfunc()
+{
+  document.getElementById("outid").innerHTML = "Outside Hello World!!!";
+}
+
+//module.exports. outfunc = outfunc
