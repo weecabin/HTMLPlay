e40635ead116027579e7ab7a4d59a20c7fdd3191
play.js <git@gituser.com>
2020-12-24 13:16:18 -0800

    deleted index.js

--- indexNotNeeded.js
+++ indexNotNeeded.js
@@ -2,11 +2,11 @@ const http = require('http');
 const fs = require('fs');
 const map= require("./map")
 
-/*
+
 http.createServer(function (req, res) {
 fs.readFile('index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
   });
-}).listen(8080);*/
+}).listen(8080);
